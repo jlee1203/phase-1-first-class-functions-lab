571f745e839bf119ce86b4fc80cc7f8fa0e9db58
James Lee <j.lee1203@hotmail.com>
2022-08-17 16:56:12 -0700

    commit tree 21241b41e191bd9a98f41bca3409f7f1cdcb4c28

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const scuberDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(scuberDrivers){
+    return scuberDrivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(scuberDrivers){
+    return scuberDrivers.slice(2,4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(number){
+    return function(num2){
+    return number * num2;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(scuberDrivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(scuberDrivers);
+}
